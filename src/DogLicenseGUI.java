abd5bfb3bfc81a323dfa42f0436723fdca392af4
jpost <jpostell29@gmail.com>
2021-07-23 21:02:14 -0700

    done

--- src/DogLicenseGUI.java
+++ src/DogLicenseGUI.java
@@ -7,39 +7,44 @@
  */
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class DogLicenseGUI {
 
 	public static void main(String[] args) {
+		// Frame
 		JFrame frame = null; // Initialize frame
 		JLabel yearLabel = null; // Initialize year label
 		JLabel userId = null; // Initialize user ID
 		JLabel licenseNum = null; // Initialize license number
+		// Text Fields
 		JTextField yearField = null; // Initialize year field display
 		JTextField userField = null; // display user id number
 		JTextField licenseField = null; // display assigned license number
+		// Submit Button
 		JButton submitButton = null; // submit button
-		GridBagConstraints layout = null; // GUI layout
+		// GridBag layout
 
+
+		// Instantiate objects
+		frame = new JFrame("Dog License Number Generator"); // creates frame w/title
+		frame.setLayout(new GridBagLayout());
 		yearLabel = new JLabel("Enter User Year:"); // set year label
 		userId = new JLabel("Enter User ID:"); // set user id label
 		licenseNum = new JLabel("Assigned Dog License:"); // set license number
 		submitButton = new JButton("Submit");
-
 		yearField = new JTextField(10);
 		yearField.setEditable(true);
-
 		userField = new JTextField(10);
 		userField.setEditable(true);
-
 		licenseField = new JTextField(10);
 		licenseField.setEditable(true);
 
-		frame = new JFrame("Dog License Number Generator"); // creates frame w/title
-		frame.setLayout(new GridBagLayout());
 
-		layout = new GridBagConstraints();
+		//
+		GridBagConstraints layout = new GridBagConstraints();
 		layout.gridx = 0;
 		layout.gridy = 0;
 		layout.insets = new Insets(10, 10, 10, 10);
@@ -81,30 +86,33 @@ public class DogLicenseGUI {
 		layout.insets = new Insets(10, 10, 10, 10);
 		frame.add(submitButton, layout);
 
+
 		frame.setVisible(true); // displays frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exits window when closed
 		frame.pack(); // resize window to fit components
 
+		JTextField finalUserField = userField;
+		JTextField finalYearField = yearField;
+		JTextField finalLicenseField1 = licenseField;
+		submitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String val1 = finalUserField.getText();
+				String val2 = finalYearField.getText();
+				String lisnum = val1 + val2;
+				finalLicenseField1.setText(lisnum + " - 21");
+			}
+		});
+
+
+
+
+
+
+
+
+
+
+
+
 	}
-
-	/*
-	TODO: Create a DogLicense class
-	 */
-	// Template for dog license
-	static class DogLicense {
-		String year;
-		int id;
-
-	}
-
-	// Function to create dog license
-	static String submit(String year, int id) {
-		String assignedNumber;
-		assignedNumber = year + id;
-		return assignedNumber;
-	}
-
-	/*
-	TODO: Create a function to take input and create a new DogLicense object
-	 */
 }
